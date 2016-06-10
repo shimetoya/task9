23ad1d35bba23fbbc1a49870d54b6da2e4470457
LenaU <shimetoya@gmail.com>
2016-06-10 11:29:23 +0300

    Class Train

---
+++ train.rb
@@ -0,0 +1,42 @@
+class Train
+	def initialize(name, type, amount_railway_carriage, speed=0)
+		puts "Номер поезда: #{name}, тип поезда: #{type}, количество вагонов у поезда: #{amount_railway_carriage}"
+		@name = name
+		@type = type
+		@amount_railway_carriage = amount_railway_carriage
+		@current_station 
+	end
+	def gather_speed
+		@speed = 60
+	end
+	def current_speed
+		puts "Текущая скорость: #{@speed}"
+	end
+	def brake
+		self.speed = 0
+	end
+	def amount_railway_carriage
+		puts "Количество вагонов: #{@amount_railway_carriage}"
+	end
+	def operation_with_railway_carriage(key)
+		if speed == 0
+			if key == '+'
+				@amount_railway_carriage += 1
+			elsif key == '-'
+				@amount_railway_carriage -= 1
+			end
+		else
+			puts "Невозможно отцепить вагон, пока поезд движется!"	
+		end
+	end
+	def route
+		puts "Поезд #{name} находится на станции: #{@current_station}"
+	end
+	def change_station(name_of_station)
+		@current_station = name_of_station
+		puts "Поезд переместился на станцию: #{@current_station}!"
+	end
+	def show_station(route)
+		puts "Список всех станций: #{route.stations}"
+	end
+end
