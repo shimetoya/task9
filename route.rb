dc050777272943a0f4e1b0a1553a024219bb6bad
LenaU <shimetoya@gmail.com>
2016-06-14 12:12:05 +0300

    добавила метод добавленипя новой станции, исправила показ всех станций

--- route.rb
+++ route.rb
@@ -1,19 +1,27 @@
 class Route
-	attr_accessor :stations	
+	attr_accessor :stations
+	attr_accessor :station
 	def initialize(first_station, last_station)
 		puts "Маршрут лежит от станции #{first_station} до станции #{last_station}."
 		@first_station = RailwayStation.new(first_station)
 		@last_station = RailwayStation.new(last_station)
 		@stations = [@first_station, @last_station]
+		
+	end
+	def new_station(station)
+		@station = RailwayStation.new(station)
 	end
 	def add_station(station)
-		station = RailwayStation.new(station)
-		@stations.insert(1, station)
+		@stations.insert(1, @station)
 	end
 	def delete_station(station)
-		@stations.delete(station)
+		@stations.delete(@station)
 	end
-	def show_stations(rail)
-		puts "Список всех станций:#{@stations}" 		
+	def show_stations
+			puts "Список всех станций:"
+		for station in @stations do
+    	print "#{station.station}, " 
+		end
+			puts " "
 	end
 end
