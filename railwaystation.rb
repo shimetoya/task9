1a6d3aed839308158f7f9d1aab4ed678a778348a
LenaU <shimetoya@gmail.com>
2016-06-14 14:13:10 +0300

    tab

--- railwaystation.rb
+++ railwaystation.rb
@@ -1,30 +1,30 @@
 class RailwayStation
-	attr_accessor :station
-	def initialize(station)
-		puts "Станция называется: #{station}"
-		@station = station
-		@current_station = station
-		@trains = []
-	end
-	def get_train(train)
-		@trains << train
-		puts "Пришел поезд: #{train.number}"
-	end
-	def current_train
-		puts "Cписок всех поездов на станции, находящиеся в текущий момент:"
-		for number in @trains do
-			print "#{number.number}, #{number.type}, " 
-		end
-		puts " "
-	end
-	def current_train_with_type_pass
-		@trains.each {|number| puts "Cписок всех поездов пассажирского типана станции, находящиеся в текущий момент:: #{number.number}, #{number.type}" if number.type == 'pass' }
-	end
-	def current_train_with_type_gruz(train)
-		@trains.each {|number| puts "Cписок всех поездов грузового типа на станции, находящиеся в текущий момент:: #{number.number}, #{number.type}" if number.type == 'gruz' }
-	end
-	def sent_train(train)
-		puts "Ушел поезд: #{train.number}"
-		@trains.delete(train)
-  end	  
+  attr_accessor :station
+  def initialize(station)
+    puts "Станция называется: #{station}"
+    @station = station
+    @current_station = station
+    @trains = []
+  end
+  def get_train(train)
+    @trains << train
+    puts "Пришел поезд: #{train.number} "
+  end
+  def current_train
+    puts "Cписок всех поездов на станции, находящиеся в текущий момент:"
+    for number in @trains do
+      print "#{number.number}, #{number.type}, " 
+    end
+    puts " "
+  end
+  def current_train_with_type_pass
+    @trains.each {|number| puts "Cписок всех поездов пассажирского типана станции, находящиеся в текущий момент:: #{number.number}, #{number.type}" if number.type == 'pass' }
+  end
+  def current_train_with_type_gruz(train)
+    @trains.each {|number| puts "Cписок всех поездов грузового типа на станции, находящиеся в текущий момент:: #{number.number}, #{number.type}" if number.type == 'gruz' }
+  end
+  def sent_train(train)
+    puts "Ушел поезд: #{train.number}"
+    @trains.delete(train)
+  end
 end
--- route.rb
+++ route.rb
@@ -1,24 +1,24 @@
 class Route
-	attr_accessor :stations
-	attr_accessor :station
-	def initialize(first_station, last_station)
-		puts "Маршрут лежит от станции #{first_station} до станции #{last_station}."
-		@first_station = RailwayStation.new(first_station)
-		@last_station = RailwayStation.new(last_station)
-		@stations = [@first_station, @last_station]
-	end
-	def add_station(rail)
-		@stations.insert(1, rail)
-		puts "Добавлена станция: #{rail.station}"
-	end
-	def delete_station(rail)
-		@stations.delete(rail)
-	end
-	def show_stations
-		puts "Список всех станций:"
-		for rail in @stations do
-			print "#{rail.station}," 
-		end
-		puts " "
-	end
+  attr_accessor :stations
+  attr_accessor :station
+  def initialize(first_station, last_station)
+    puts "Маршрут лежит от станции #{first_station} до станции #{last_station}."
+    @first_station = RailwayStation.new(first_station)
+    @last_station = RailwayStation.new(last_station)
+    @stations = [@first_station, @last_station]
+  end
+  def add_station(rail)
+    @stations.insert(1, rail)
+    puts "Добавлена станция: #{rail.station}"
+  end
+  def delete_station(rail)
+    @stations.delete(rail)
+  end
+  def show_stations
+    puts "Список всех станций: "
+    for rail in @stations do
+      print "#{rail.station}, " 
+    end
+    puts " "
+  end
 end
