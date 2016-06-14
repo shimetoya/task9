b5714dfe2d85e751ce8f81df9a3cb9cf065b5a9a
LenaU <shimetoya@gmail.com>
2016-06-14 12:13:27 +0300

    Исправила список поездов по типам и отправку поезда

--- railwaystation.rb
+++ railwaystation.rb
@@ -7,21 +7,24 @@ class RailwayStation
 			@trains = []
 	end
 	def get_train(train)
-		@trains << train.number << train.type
-		puts "Пришел поезд: #{train.number} => #{train.type}"
+		@trains << train
+		puts "Пришел поезд: #{train.number}"
 	end
 	def current_train
-		puts "Cписок всех поездов на станции, находящиеся в текущий момент: #{@trains}"
+		puts "Cписок всех поездов на станции, находящиеся в текущий момент:"
+			for number in @trains do
+    	print "#{number.number}, #{number.type}, " 
+		end
+			puts " "
 	end
-	def current_train_with_type_pass(train)
-		@trains.select{|type| puts "#{train.number}, #{train.type}" if type == 'pass' }
+	def current_train_with_type_pass
+		@trains.each {|number| puts "Cписок всех поездов пассажирского типана станции, находящиеся в текущий момент:: #{number.number}, #{number.type}" if number.type == 'pass' }
 	end
 	def current_train_with_type_gruz(train)
-		@trains.select{|type| puts "#{train.number}, #{train.type}" if type == 'gruz' }
+		@trains.each {|number| puts "Cписок всех поездов грузового типа на станции, находящиеся в текущий момент:: #{number.number}, #{number.type}" if number.type == 'gruz' }
 	end
 	def sent_train(train)
 		puts "Ушел поезд: #{train.number}"
-		@trains.delete(train.number)
-		@trains.delete(train.type)
+		@trains.delete(train)
 	end	
 end
