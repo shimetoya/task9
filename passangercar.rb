4da1571312f134fcc4c4c7a7dfa3f3e723240102
LenaU <shimetoya@gmail.com>
2016-06-14 14:17:06 +0300

    tab

--- passangercar.rb
+++ passangercar.rb
@@ -1,6 +1,6 @@
 class PassangerCar 
-	def initialize(amount_passanger_car)
-		@amount_passanger_car = amount_passanger_car
-		type = PassangerTrain		
-	end
+  def initialize(amount_passanger_car)
+    @amount_passanger_car = amount_passanger_car
+    type = PassangerTrain		
+  end
 end
