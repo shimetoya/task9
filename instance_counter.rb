d96ac9761a4aab996f1523ca511d0997690e6a13
LenaU <shimetoya@gmail.com>
2016-06-24 16:14:35 +0300

    не обнуляет счетчик

--- instance_counter.rb
+++ instance_counter.rb
@@ -2,19 +2,22 @@ module InstanceCounter
   def self.included(base)
     base.extend ClassMethods
     base.send :include, InstanceMethods
+    @@counter = 1
+    base.class_variable_set(:@@counter, 0)
   end
   module ClassMethods
     def instances
-      counte = InstanceMethods.class_variable_get(:@@counte)
-      puts "Количество экземпляров класса: #{counte}"
+      counter = InstanceCounter.class_variable_get(:@@counter)
+      puts "Количество экземпляров класса: #{counter}"
     end
   end
 
   module InstanceMethods
     protected
-    @@counte=0 
     def register_instance
-      @@counte +=1  
+      @@counter = InstanceCounter.class_variable_get(:@@counter)
+      @@counter += 1
+      self.class.class_variable_set(:@@counter, @@counter)
     end
   end
 end 
