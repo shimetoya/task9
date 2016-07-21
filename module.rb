5c1f5dd7c0dae263c59e891733e509352cf11268
LenaU <shimetoya@gmail.com>
2016-07-21 15:37:52 +0300

    module

--- module.rb
+++ module.rb
@@ -1,13 +1,31 @@
-module Company
-  def set_name(name)
-    @company_name = name
+ Выполнить следующие задачи:
+
+    Написать модуль, содержащий 2 метода, которые можно вызывать на урвне класса:
+        метод att_accessor_with_history. Динамически создает геттеры и сеттеры для любого кол-ва атрибутов, при этом сеттер сохраняет все значения инстанс-переменной
+при изменении этого значения. Также должен быть метод <имя_атрибута>_history, который возвращает массив всех значений данной переменной.
+
+        метод strong_attr_acessor, который принимает имя атрибута и его класс. При этом создается геттер и сеттер для одноименной инстанс-переменной, но сеттер проверяет
+тип присваемоего значения. Если тип отличается от того, который указан вторым параметром, то выбрасывается исключение. Если тип совпадает, то значение присваивается.
+    Подключить модуль в класс и продемонстрировать применение этих методов.
+
+class Module
+  def att_accessor_with_history(*names)
+    names.each do |name|
+      var_name = "@#{name}".to_sym
+      define_method(name) { instance_variable_get(var_name) }
+      define_method("#{name}=".to_sym) { |value| instance_variable_set(var_name, value) }
+      @array << self
+    end
   end
-
-  def companys_name
-    @company_name
+  def name_history
+    @array
   end
-
-  protected
-
-  attr_accessor :company_name
+  def strong_attr_acessor(*names, *names.class)
+    names.each do |name|
+      var_name = "@#{name}".to_sym
+      define_method(name) { instance_variable_get(var_name) }
+      define_method("#{name}=".to_sym) { |value| if var_name.type = instance_variable_set(var_name, value) }
+      @array << self
+    end
+  end
 end
