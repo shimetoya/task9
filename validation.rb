27fa103ac2ed726eb3134585aa8e159e14d73d1c
LenaU <shimetoya@gmail.com>
2016-07-25 20:59:08 +0300

    исправила 1 замечание

--- validation.rb
+++ validation.rb
@@ -1,5 +1,4 @@
 module Validation
-
   def self.included(base)
     base.extend ClassMethods
     base.send :include, InstanceMethods
@@ -7,17 +6,23 @@ module Validation
 
   module ClassMethods
     def validate(name, types, *arg)
-      @name = name
-      @types = types
-      @arg = arg
+       @name = name
+       @types = types
+       @arg = arg
     end
   end
 
   module InstanceMethods
-    def validate! 
-      @types == presence ? (@name != nil || @name != "") : raise('Name cant be empty')
-      @types == format ? @arg == FORMAT : raise('Arg must be format')
-      @types == type ? @types == @name.class : raise('Type must be')
+    attr_accessor :types, :name, :arg, :val
+    def validate!
+      case
+        when @types == 'presence' 
+         (@name == nil || @name == "") ? raise('Name cant be empty') : return
+        when @types == 'format' 
+          @name !~ @arg ? raise('Arg must be format') : return
+        when @types == 'type' 
+          @arg != @name.class ? raise('Type must be') : return
+      end
     end
 
     def valid?
@@ -30,16 +35,29 @@ end
 
 class Test
   include Validation
-  attr_accessor :number, :name
-  FORMAT = /A-Z{0,3}/
+  attr_accessor :number, :name, :presence, :format, :type
 
-  validate :name, :presence
-  validate :number, :format, FORMAT
-  validate :name, :type, String
+  #validate :name, :types, :arg
+  #validate :name, :format, FORMAT
+  validate :name, :type, :arg
 end
 
+FORMAT = /^[a-z\d]{3}-?[a-z\d]{2}$/i
+
 t = Test.new
-t.number = 7
-t.valid?
+# t.name = '118-gF'
+# t.name = '1gF'
+# t.types = 'format'
+# t.arg = FORMAT
+# t.validate!
 
+# t.name = "yuio"
+# t.name = ""
+# t.types = 'presence'
+# t.validate!
 
+# t.name = "yuio"
+t.name = 10
+t.types = 'type'
+t.arg = String
+t.validate!
