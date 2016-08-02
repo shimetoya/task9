4c3c37a3415a0f9595e54d7e9f0d05d6c28fd220
shimetoya <shimetoya@gmail.com>
2016-08-02 15:34:17 +0300

    task9

--- validation.rb
+++ validation.rb
@@ -5,24 +5,30 @@ module Validation
   end
 
   module ClassMethods
-    def validate(name, types, *arg)
-       @name = name
-       @types = types
-       @arg = arg
+    def validate(name, rule, arg = nil)
+      class_variable_set(:@@validate_rules, []) unless class_variables.include?(:@@validate_rules)
+      class_variable_get(:@@validate_rules) << {
+        name: name,
+        rule: rule,
+        arg: arg
+      }
     end
   end
 
   module InstanceMethods
-    attr_accessor :types, :name, :arg, :val
     def validate!
-      case
-        when @types == 'presence' 
-         (@name == nil || @name == "") ? raise('Name cant be empty') : return
-        when @types == 'format' 
-          @name !~ @arg ? raise('Arg must be format') : return
-        when @types == 'type' 
-          @arg != @name.class ? raise('Type must be') : return
+      self.class.class_variable_get(:@@validate_rules).each do |rule|
+        val = instance_variable_get("@#{rule[:name]}".to_sym)
+        case rule[:rule]
+          when :presence
+            raise'Name cant be empty' unless val
+          when :format
+            raise'Arg must be format' unless val =~ rule[:arg]
+          when :type
+            raise'Type must be identical' unless val.instance_of?(rule[:arg])
+        end
       end
+      true
     end
 
     def valid?
@@ -35,29 +41,12 @@ end
 
 class Test
   include Validation
-  attr_accessor :number, :name, :presence, :format, :type
-
-  #validate :name, :types, :arg
-  #validate :name, :format, FORMAT
-  validate :name, :type, :arg
+  attr_accessor :a, :b
+  validate :a, :presence 
+  validate :b, :type, String
 end
 
-FORMAT = /^[a-z\d]{3}-?[a-z\d]{2}$/i
-
 t = Test.new
-# t.name = '118-gF'
-# t.name = '1gF'
-# t.types = 'format'
-# t.arg = FORMAT
-# t.validate!
-
-# t.name = "yuio"
-# t.name = ""
-# t.types = 'presence'
-# t.validate!
-
-# t.name = "yuio"
-t.name = 10
-t.types = 'type'
-t.arg = String
+t.a = 'aaa'
+t.b = '111'
 t.validate!
